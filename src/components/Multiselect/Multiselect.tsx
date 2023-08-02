97b65686fc030f242b0d806d3773965571774307
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2023-08-03 01:32:00 +0600

    implement remove

--- src/components/Multiselect/Multiselect.tsx
+++ src/components/Multiselect/Multiselect.tsx
@@ -1,49 +1,89 @@
-import { useState } from 'react'
-type OptionType = {
-    value: string;
-    label: string;
-}
+import { useState } from "react";
 
 const Multiselect = () => {
+  const [show, setShow] = useState(false);
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+  const [options, setOptions] = useState<Array<T>>([
+    { value: "Red", label: "Red" },
+    { value: "Black", label: "Black" },
+    { value: "Yellow", label: "Yellow" },
+    { value: "Green", label: "Green" },
+    { value: "Purple", label: "Purple" },
+  ]);
 
-    const [selectedOptions, setSelectedOptions] = useState([])
+  const handleOption = (label: string): void => {
+    setSelectedOptions((prev) => [...prev, label]);
+    setOptions((prev) => prev.filter((option) => option.label !== label));
+  };
 
-    const options: OptionType[] = [
-        { value: 'red', label: 'Red' },
-        { value: 'black', label: 'Black' },
-        { value: 'yellow', label: 'Yellow' },
-        { value: 'green', label: 'Green' },
-        { value: 'purple', label: 'Purple' },
-    ]
+  const handleRemove = (option: string): void => {
+    const remainOptions = selectedOptions.filter((op) => op !== option);
+    setSelectedOptions(remainOptions);
+    const reAdd = selectedOptions.find((op) => op == option);
+    setOptions((prev) => [{ value: reAdd, label: reAdd }, ...prev]);
+  };
 
-    const handleOption = (label: string) => {
-        setSelectedOptions(prev => [...prev, label])
-        conso
-    }
+  const handleAllRemove = ():void => {
+    setSelectedOptions([]);
+    setOptions([
+      { value: "Red", label: "Red" },
+      { value: "Black", label: "Black" },
+      { value: "Yellow", label: "Yellow" },
+      { value: "Green", label: "Green" },
+      { value: "Purple", label: "Purple" },
+    ]);
+  };
 
-    return (
-        <div className="w-96">
-
-            <div className="w-full py-2 px-3 flex flex-wrap border border-red-600">
-                {
-                    selectedOptions.map((option) => (
-                        <div className='bg-black m-1 px-1 text-sm flex items-center justify-center rounded text-white' key={option}>{option}</div>
-                    ))
-                }
-                <div className='flex-1'>
-                    <input className="w-full border-none focus:outline-none h-full px-3 py-2" type="text" name="" id="" placeholder="text" />
-                </div>
-            </div>
-
-            <div className="w-full mt-2 border rounded-lg flex flex-col justify-start items-start">
-                {
-                    options.map(({ value, label }) => (
-                        <div onClick={() => handleOption(label)} className="w-full text-left px-3 py-2 hover:bg-gray-100 rounded-lg cursor-pointer" key={value}>{label}</div>
-                    ))
-                }
-            </div>
+  return (
+    <div className="w-96">
+      <div className="w-full py-2 px-3 flex flex-wrap border border-red-600">
+        {selectedOptions.map((option) => (
+          <div
+            onClick={() => handleRemove(option)}
+            className="bg-black m-1 px-1 text-sm flex items-center cursor-pointer justify-center rounded text-white"
+            key={option}
+          >
+            {option}
+          </div>
+        ))}
+        <div className="flex-1 flex justify-between items-center">
+          <input
+            className="w-full border-none focus:outline-none h-full px-3 py-2"
+            type="text"
+            name=""
+            id=""
+            placeholder="Search"
+          />
+          <button
+            onClick={handleAllRemove}
+            className="text-2xl font-bold px-2 text-red-500"
+          >
+            &#215;
+          </button>
+          <div
+            onClick={() => setShow(!show)}
+            className="px-2 border-l border-gray-500"
+          >
+            <button className="text-2xl text-gray-500 font-bold">&#94;</button>
+          </div>
         </div>
-    );
+      </div>
+
+      {show && (
+        <div className="w-full mt-2 border rounded-lg flex flex-col justify-start items-start">
+          {options.map(({ value, label }) => (
+            <div
+              onClick={() => handleOption(label)}
+              className="w-full text-left px-3 py-2 hover:bg-gray-100 rounded-lg cursor-pointer"
+              key={value}
+            >
+              {label}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Multiselect;
--- src/components/Title/Title.test.ts
+++ src/components/Title/Title.test.ts
@@ -1,7 +1,7 @@
-// import { render, screen } from '@testing-library/react';
-// import Title from './Title';
-// test('fdf',()=>{
-//   render(<Title/>);
-//   const titleTextEl=screen.getByText(/this is title/i);
-//   expect(titleTextEl).toBeInTheDocument()
-// })
+import { render, screen } from '@testing-library/react';
+import Title from './Title';
+test('fdf',()=>{
+  render(<Title/>);
+  const titleTextEl=screen.getByText(/this is title/i);
+  expect(titleTextEl).toBeInTheDocument()
+})
